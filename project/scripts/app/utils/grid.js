8edbf4799e35fa615b4ad0989a6bfe6348a288c1
Mark <mark.a.bos@gmail.com>
2018-06-11 09:20:40 +0200

    edit grid layout

--- project/scripts/app/utils/grid.js
+++ project/scripts/app/utils/grid.js
@@ -8,7 +8,7 @@ function toggle() {
   if (i % 2 === 0) {
     var $grid = document.createElement('div');
     $grid.classList.add('demo-grid-overlay');
-    $grid.innerHTML = '<div class="demo-grid"><div class="demo-grid__item"><span>1</span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span>4</span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span>8</span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span>12</span></div></div>';
+    $grid.innerHTML = '<div class="demo-grid"><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div><div class="demo-grid__item"><span></span></div></div>';
     document.body.appendChild($grid);
   } else {
     document.body.removeChild(document.querySelector('.demo-grid-overlay'));
